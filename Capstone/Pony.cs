c0f089403019ea4cc9aaf57d28f52ec1a9a7e740
Dale Mitchell <Dalesree@gmail.com>
2023-02-09 12:33:00 -0500

    Added setters to each animal

--- Capstone/Pony.cs
+++ Capstone/Pony.cs
@@ -5,8 +5,15 @@ using System.Text;
 namespace Capstone
 {
     public class Pony : StuffedAnimals
-    { 
+    {
+
         public string AnimalSound { get; } = "Neigh, Neigh, Yay!";
+        public Pony(string slot, string name, decimal price)
+        {
+            Slot = slot;
+            Name = name;
+            Price = price;
+        }
 
     }
 }
